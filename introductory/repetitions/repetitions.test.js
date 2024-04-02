8143d80df06a02f9dd385e74f782567f41760545
Stanley <27.stanleylch@gmail.com>
2024-04-02 13:02:29 +0100

    complete repetitions problem with tdd

--- introductory/repetitions/repetitions.test.js
+++ introductory/repetitions/repetitions.test.js
@@ -9,16 +9,21 @@ describe("mrepetitions problem", () => {
   test("return integer 1 when input is 'AC'", () => {
     const input = "AC";
     const output = 1;
-    expect(repetitions(input).toBe(output));
+    expect(repetitions(input)).toBe(output);
   });
   test("return integer 1 when input is 'ACTG'", () => {
     const input = "ACTG";
     const output = 1;
-    expect(repetitions(input).toBe(output));
+    expect(repetitions(input)).toBe(output);
   });
   test("return integer 3 when input is 'AAA'", () => {
     const input = "AAA";
     const output = 3;
-    expect(repetitions(input).toBe(output));
+    expect(repetitions(input)).toBe(output);
+  });
+  test("return integer 3 when input is 'ATTCGGGA'", () => {
+    const input = "AATTCGGGA";
+    const output = 3;
+    expect(repetitions(input)).toBe(output);
   });
 });
