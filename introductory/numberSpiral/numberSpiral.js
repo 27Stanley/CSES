59b6e2d33e1ed1ba0bd676e49b18bfa76fac98bb
Stanley <27.stanleylch@gmail.com>
2024-04-09 18:31:34 +0100

    number spiral problem complete with tdd

--- introductory/numberSpiral/numberSpiral.js
+++ introductory/numberSpiral/numberSpiral.js
@@ -1,5 +1,23 @@
-function numberSpiral() {
-  return 1;
+function numberSpiral(x, y) {
+  if (x === 1 && y === 1) {
+    return 1;
+  }
+
+  if (x === y) {
+    return x * x - (x - 1);
+  } else if (x > y && x % 2 === 0) {
+    const diagNum = x * x - (x - 1);
+    return diagNum + (x - y);
+  } else if (x > y && x % 2 !== 0) {
+    const diagNum = x * x - (x - 1);
+    return diagNum - (x - y);
+  } else if (y > x && y % 2 === 0) {
+    const diagNum = y * y - (y - 1);
+    return diagNum - (y - x);
+  } else {
+    const diagNum = y * y - (y - 1);
+    return diagNum + (y - x);
+  }
 }
 
 module.exports = numberSpiral;
--- introductory/numberSpiral/numberSpiral.test.js
+++ introductory/numberSpiral/numberSpiral.test.js
@@ -2,8 +2,41 @@ const numberSpiral = require("./numberSpiral");
 
 describe("number spiral problem", () => {
   test("if x and y are equal", () => {
-    const input = (5, 5);
-    const ouptut = 21;
-    expect(numberSpiral(input)).toBe(output);
+    const inputRow = 5;
+    const inputCol = 5;
+    const output = 21;
+    expect(numberSpiral(inputRow, inputCol)).toBe(output);
+  });
+
+  test("if x > y and x is odd", () => {
+    const inputRow = 5;
+    const inputCol = 4;
+    const output = 20;
+    expect(numberSpiral(inputRow, inputCol)).toBe(output);
+  });
+
+  test("if x > y and x is even", () => {
+    const inputRow = 4;
+    const inputCol = 3;
+    const output = 14;
+    expect(numberSpiral(inputRow, inputCol)).toBe(output);
+  });
+  test("if y > x and y is even", () => {
+    const inputRow = 3;
+    const inputCol = 4;
+    const output = 12;
+    expect(numberSpiral(inputRow, inputCol)).toBe(output);
+  });
+  test("if y > x and y is odd", () => {
+    const inputRow = 3;
+    const inputCol = 5;
+    const output = 23;
+    expect(numberSpiral(inputRow, inputCol)).toBe(output);
+  });
+  test("if x and y are 1", () => {
+    const inputRow = 1;
+    const inputCol = 1;
+    const output = 1;
+    expect(numberSpiral(inputRow, inputCol)).toBe(output);
   });
 });
